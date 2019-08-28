99bcdd938ca8dcd6b13e10c9047a96c36aa43664
DasSauerkraut <cspudsy@yahoo.com>
2019-08-28 14:20:16 -0500

    updated girignore initial scripting for onager gun

--- jars/src/data/scripts/weapons/AlmainOnagerCannon.java
+++ jars/src/data/scripts/weapons/AlmainOnagerCannon.java
@@ -14,7 +14,7 @@ public class AlmainOnagerCannon {
     private final String CHARGE_SOUND_ID = "almain_onager_charge";
     private final String FIRE_SOUND_ID = "almain_onager_fire";
 
-    private boolean runOnce = false;
+    private boolean hasFired = false;
 
     public void advance(float amount, CombatEngineAPI engine, DamagingProjectileAPI shell) {
         // Don't bother with any unecessary checks
@@ -26,23 +26,23 @@ public class AlmainOnagerCannon {
         ShipAPI ship = weapon.getShip();
 
         //Plays charge sound and spawn some charge-y particles
-        if (!runOnce && !engine.isPaused()) {
+        if (!hasFired && !engine.isPaused() && weapon.getChargeLevel() > 0f) {
             Global.getSoundPlayer().playLoop(CHARGE_SOUND_ID, ship, 1f, (0.5f + (weapon.getChargeLevel() * 0.5f)), weapon.getLocation(), new Vector2f(0f, 0f));
         }
 
         //Actually fire the weapon!
-        if (weapon.getChargeLevel() >= 1f && !runOnce) {
-            runOnce = true;
+        if (weapon.getChargeLevel() >= 1f && !hasFired) {
+            hasFired = true;
             //For spawning the sounds at the end, middle and start of the beam
             Global.getSoundPlayer().playSound(FIRE_SOUND_ID, 1f, 1.25f, weapon.getLocation(), new Vector2f(0f, 0f));
 
             //Muzzle flash
             engine.addHitParticle(weapon.getLocation(), ship.getVelocity(), 325f, 0.22f, 1.9f, MUZZLE_FLASH_COLOR_BRIGHT);
             engine.addHitParticle(weapon.getLocation(), ship.getVelocity(), 850f, 0.13f, 2f, MUZZLE_FLASH_COLOR);
-            }
+        }
 
-        if (!weapon.isFiring()) {
-            runOnce = false;
+        if (hasFired && weapon.getChargeLevel() == 0f) {
+            hasFired = false;
         }
     }
 }
--- jars/src/data/scripts/weapons/almainOnagerCannon.java
+++ jars/src/data/scripts/weapons/almainOnagerCannon.java
@@ -2,18 +2,20 @@ package data.scripts.weapons;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
-import com.fs.starfarer.api.graphics.SpriteAPI;
-import data.scripts.plugins.MagicTrailPlugin;
-import org.lazywizard.lazylib.MathUtils;
 import org.lwjgl.util.vector.Vector2f;
 
 import java.awt.*;
 
-public class almainOnagerCannon implements {
-    private final String CHARGE_SOUND_ID = "almain_onager_charge";
-    private boolean runOnce = false;
+public class AlmainOnagerCannon {
 
-    @Override
+    private static final Color MUZZLE_FLASH_COLOR_BRIGHT = new Color(255, 165, 158, 255);
+    private static final Color MUZZLE_FLASH_COLOR = new Color(255, 0, 0, 255);
+
+    private final String CHARGE_SOUND_ID = "almain_onager_charge";
+    private final String FIRE_SOUND_ID = "almain_onager_fire";
+
+    private boolean hasFired = false;
+
     public void advance(float amount, CombatEngineAPI engine, DamagingProjectileAPI shell) {
         // Don't bother with any unecessary checks
         if (shell.getWeapon().getShip() == null) {
@@ -23,8 +25,24 @@ public class almainOnagerCannon implements {
         WeaponAPI weapon = shell.getWeapon();
         ShipAPI ship = weapon.getShip();
 
-        if(!runOnce && !engine.isPaused()) {
-            Global.getSoundPlayer().playLoop(CHARGE_SOUND_ID, ship, (0.65f + weapon.getChargeLevel()*2f), (0.5f + (weapon.getChargeLevel() * 0.5f)), beam.getFrom(), new Vector2f(0f, 0f));
+        //Plays charge sound and spawn some charge-y particles
+        if (!hasFired && !engine.isPaused() && weapon.getChargeLevel() > 0f) {
+            Global.getSoundPlayer().playLoop(CHARGE_SOUND_ID, ship, 1f, (0.5f + (weapon.getChargeLevel() * 0.5f)), weapon.getLocation(), new Vector2f(0f, 0f));
+        }
+
+        //Actually fire the weapon!
+        if (weapon.getChargeLevel() >= 1f && !hasFired) {
+            hasFired = true;
+            //For spawning the sounds at the end, middle and start of the beam
+            Global.getSoundPlayer().playSound(FIRE_SOUND_ID, 1f, 1.25f, weapon.getLocation(), new Vector2f(0f, 0f));
+
+            //Muzzle flash
+            engine.addHitParticle(weapon.getLocation(), ship.getVelocity(), 325f, 0.22f, 1.9f, MUZZLE_FLASH_COLOR_BRIGHT);
+            engine.addHitParticle(weapon.getLocation(), ship.getVelocity(), 850f, 0.13f, 2f, MUZZLE_FLASH_COLOR);
+        }
+
+        if (hasFired && weapon.getChargeLevel() == 0f) {
+            hasFired = false;
         }
     }
 }
